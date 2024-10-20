2c9fa8a31322bfb72de23be19056858cebe27afc
raghavmanish <mnsyd24@gmail.com>
2024-10-21 01:25:08 +0530

    Added proxy (reverse proxy)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const {rateLimit} = require("express-rate-limit") ;
+
+const { rateLimit } = require("express-rate-limit") ;
+const { createProxyMiddleware } = require("http-proxy-middleware") ;
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -16,6 +18,30 @@ app.use(express.json()) ;
 app.use(express.urlencoded({extended:true})) ;
 
 app.use(limiter) ;
+
+/**
+ *                                       user
+ *                                        |
+ *                                        v
+ *                            localhost:3001 (API Gateway)
+ *                                        |
+ *                                        v
+ *        localhost:3000/api/v1/flights       localhost:4000/api/v1/bookings
+ */
+ 
+// localhost:5000/flightService/api/get/info =====> localhost:3000/api/get/info
+app.use("/flightService" , createProxyMiddleware({
+    target : ServerConfig.FLIGHT_SERVICE ,
+    changeOrigin : true ,
+    pathRewrite : {'^/flightService' : '/'} , 
+}))
+
+// localhost:5000/bookinservice/api/get/info =====> localhost:4000/api/get/info
+app.use("/bookingService" , createProxyMiddleware({//             A
+    target : ServerConfig.BOOKING_SERVICE , //                    |
+    changeOrigin : true ,   //                                    |
+    pathRewrite : {'^/bookingService' : '/'} , // this line of code is responsible for the above conversion 
+}))
 
 app.use('/api', apiRoutes);
 
